e1bf2be8b0a551b34ad983b20a2d9662f06619d7
Gerrie Myburgh <gerrie.myburgh@yahoo.co.uk>
2025-11-07 18:36:19 +0200

    Changes to call the get-comments exe on linux end windows

--- main.ts
+++ main.ts
@@ -1,89 +1,149 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	FileSystemAdapter,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
+import { spawn } from "child_process";
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	dir: string;
+	work: string;
+	start: string;
+	path: string;
+	extension: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	dir: "UNKNOWN",
+	work: "UNKNOWN",
+	start: "UNKNOWN",
+	path: "UNKNOWN",
+	extension: "UNKNOWN",
+};
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	app: App;
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+		const ribbonIconEl = this.addRibbonIcon(
+			"eye",
+			"Scan text files for comment lines",
+			(_evt: MouseEvent) => {
+				if (this.settings.dir == "UNKNOWN") {
+					new Notice("Please configure plugin before using");
+					return;
+				}
+				const platform = process.platform; // e.g., 'darwin', 'win32', 'linux'
+				const adapter = this.app.vault.adapter;
+				const parameters = [
+					"-dir",
+					this.settings.dir,
+					"-start",
+					this.settings.start,
+					"-path",
+					this.settings.path,
+					"-ext",
+					this.settings.extension,
+				];
+				if (adapter instanceof FileSystemAdapter) {
+					if (platform === "win32") {
+						const basePath =
+							adapter.getBasePath() +
+							"/.obsidian/plugins/code-scanner-ver2/binaries/windows";
+						let workFolder = "";
+						if (this.settings.work.startsWith("\\")) {
+							workFolder = this.settings.work;
+						} else {
+							workFolder = "\\" + this.settings.work;
+						}
+						const child = spawn(
+							basePath + "\\get-comments.exe",
+							parameters.concat([
+								"-work",
+								adapter.getBasePath() + workFolder,
+							]),
+						);
+
+						child.stdout.on("data", (data) => {
+							console.log(`stdout: ${data}`);
+						});
+
+						child.stderr.on("data", (data) => {
+							console.log(`stderr: ${data}`);
+						});
+
+						child.on("close", (code) => {
+							console.log(
+								`child process exited with code ${code}`,
+							);
+						});
+					} else if (platform === "darwin") {
+						console.log("Running on macOS");
+					} else if (platform === "linux") {
+						const basePath =
+							adapter.getBasePath() +
+							"/.obsidian/plugins/code-scanner-ver2/binaries/linux";
+						let workFolder = "";
+						if (this.settings.work.startsWith("/")) {
+							workFolder = this.settings.work;
+						} else {
+							workFolder = "/" + this.settings.work;
+						}
+						const child = spawn(
+							basePath + "/get-comments",
+							parameters.concat([
+								"-work",
+								adapter.getBasePath() + workFolder,
+							]),
+						);
+
+						child.stdout.on("data", (data) => {
+							new Notice(`stdout: ${data}`);
+						});
+
+						child.stderr.on("data", (data) => {
+							console.log(`stderr: ${data}`);
+						});
+
+						child.on("close", (code) => {
+							console.log(
+								`child process exited with code ${code}`,
+							);
+						});
+					}
+				}
+				// Called when the user clicks the icon.
+			},
+		);
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		ribbonIconEl.addClass("my-plugin-ribbon-class");
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		statusBarItemEl.setText("Status Bar Text");
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData(),
+		);
 	}
 
 	async saveSettings() {
@@ -97,12 +157,12 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.setText("Woah!");
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -113,22 +173,73 @@ class SampleSettingTab extends PluginSettingTab {
 	constructor(app: App, plugin: MyPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
+		this.app = app;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Folder")
+			.setDesc("Location of text file to scan")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your text file start folder")
+					.setValue(this.plugin.settings.dir)
+					.onChange(async (value) => {
+						this.plugin.settings.dir = value;
+						await this.plugin.saveSettings();
+					}),
+			);
+		new Setting(containerEl)
+			.setName("Working folder")
+			.setDesc("Location of md files")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your working folder name")
+					.setValue(this.plugin.settings.work)
+					.onChange(async (value) => {
+						this.plugin.settings.work = value;
+						await this.plugin.saveSettings();
+					}),
+			);
+		new Setting(containerEl)
+			.setName("Start")
+			.setDesc("The start of line to extract to md file")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your start string")
+					.setValue(this.plugin.settings.start)
+					.onChange(async (value) => {
+						this.plugin.settings.start = value;
+						await this.plugin.saveSettings();
+					}),
+			);
+		new Setting(containerEl)
+			.setName("MD file path definition")
+			.setDesc("The folder structure definition")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your folder structure definition")
+					.setValue(this.plugin.settings.path)
+					.onChange(async (value) => {
+						this.plugin.settings.path = value;
+						await this.plugin.saveSettings();
+					}),
+			);
+		new Setting(containerEl)
+			.setName("Extension")
+			.setDesc("Extension of text files to scan")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your text file extension")
+					.setValue(this.plugin.settings.extension)
+					.onChange(async (value) => {
+						this.plugin.settings.extension = value;
+						await this.plugin.saveSettings();
+					}),
+			);
 	}
 }
